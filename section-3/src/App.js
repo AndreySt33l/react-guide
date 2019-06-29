78c595eadbf97d80aa0fb9f7bd7a24196b8104b6
AndreySt33l <stalnoyandrey.private@gmail.com>
2019-06-29 14:36:21 +0600

    Learning in Progress

--- section-3/src/App.js
+++ section-3/src/App.js
@@ -4,24 +4,43 @@ import './App.css';
 import Person from './Person/Person.js';
 
 class App extends Component {
-  render() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1 className="App-title">Welcome to React</h1>
-            </header>
-            <p className="App-intro">
-                To get started, edit <code>src/App.js</code> and save to reload.
-            </p>
-            <h1>This is my DEMO</h1>
-            <Person/>
-            <Person/>
-            <Person/>
-            <Person/>
-        </div>
-    );
-  }
+    state = {
+        persons: [
+            { name: "Max", age: 25 },
+            { name: "Stephanie", age: 42 },
+            { name: "John", age: 24 }
+        ]
+    }
+
+    switchHandler = () => {
+        console.log("Clicked");
+        this.setState({
+            persons: [
+                { name: "Andrey", age: 23 },
+                { name: "Stephanie", age: 42 },
+                { name: "John", age: 24 }
+            ]
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <button onClick={this.switchHandler}>Switch</button>
+                <h1>This is my DEMO</h1>
+                <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}>I am the best</Person>
+                <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}>I am the best</Person>
+                <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}>I am the best</Person>
+            </div>
+        );
+    }
 }
 
 export default App;
