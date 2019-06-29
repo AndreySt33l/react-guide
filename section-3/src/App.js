d0fb10967efe34748848dbf8cc48fc744ab3eec7
Andrey Stalnoy <steel@iMac-Andrey.local>
2019-06-29 16:03:57 +0600

    Learnong on going

--- section-3/src/App.js
+++ section-3/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Person/Person.css';
 import Person from './Person/Person.js';
 
 class App extends Component {
@@ -23,7 +24,24 @@ class App extends Component {
         });
     }
 
+    nameChangedHandler = (event) => {
+        this.setState( {
+            persons: [
+                { name: 'Max', age: 25 },
+                { name: event.target.value , age: 42 },
+                { name: "John", age: 24 }
+            ]
+        })
+    }
     render() {
+        const style = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer'
+        };
+
         return (
             <div className="App">
                 <header className="App-header">
@@ -33,10 +51,20 @@ class App extends Component {
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
                 </p>
-                <button onClick={this.switchHandler}>Switch</button>
+                <button
+                    style={style}
+                    onClick={this.switchHandler}>
+                        Switch
+                </button>
                 <h1>This is my DEMO</h1>
                 <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}>I am the best</Person>
-                <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}>I am the best</Person>
+                <Person
+                    name = {this.state.persons[1].name}
+                    age = {this.state.persons[1].age}
+                    click={this.switchHandler.bind(this, 'Max!')}
+                    changed={this.nameChangedHandler}>
+                        I am the best
+                </Person>
                 <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}>I am the best</Person>
             </div>
         );
